fb59633372a99ef58a58cff2cdda3fd8f10b54ef
aleferamos <alefepdias@gmail.com>
2022-03-22 20:06:11 -0400

    interface user repository

---
+++ src/main/java/br/com/alefe/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.alefe.api.repositories;
+
+import br.com.alefe.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
