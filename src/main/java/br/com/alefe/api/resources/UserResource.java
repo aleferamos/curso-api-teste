92d156f61aff47333a66b65d2fc3332658041bec
aleferamos <alefepdias@gmail.com>
2022-03-22 20:06:28 -0400

    classe UserResource

--- src/main/java/br/com/alefe/api/resources/UserResource.java
+++ src/main/java/br/com/alefe/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.alefe.api.resources;
 
 import br.com.alefe.api.domain.User;
+import br.com.alefe.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Alefe Patrick", "alefepdias@gmail.com", "alefek159"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
