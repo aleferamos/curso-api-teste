9c5407379ec6d47b7aa9ba778c77b24e7c21d9d0
aleferamos <alefepdias@gmail.com>
2022-03-24 17:36:28 -0400

    teste para manipulador excecoes metodo DataIntegrity

--- src/main/java/br/com/alefe/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/alefe/api/resources/exceptions/ResourceExceptionHandler.java
@@ -25,7 +25,7 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(DataIntegratyViolationException.class)
-    public ResponseEntity<StandardError>dataIntegratyViolationException(DataIntegratyViolationException ex, HttpServletRequest request){
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegratyViolationException ex, HttpServletRequest request){
         StandardError error = new StandardError(
                 LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST.value(),
