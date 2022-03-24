e69ebb12373bb104eeb1ba74447469481fe8f264
aleferamos <alefepdias@gmail.com>
2022-03-24 17:36:39 -0400

    removendo anotações não utilizadas

--- src/main/java/br/com/alefe/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/alefe/api/domain/dto/UserDTO.java
@@ -3,12 +3,10 @@ package br.com.alefe.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
